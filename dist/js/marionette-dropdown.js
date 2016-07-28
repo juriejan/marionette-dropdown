7d615c7ee447068e4e941ae6addf5bbaa43191cb
Jurie-Jan Botha <juriejanbotha@gmail.com>
2016-07-28 21:19:40 +0200

    Switch to new icons system

--- dist/js/marionette-dropdown.js
+++ dist/js/marionette-dropdown.js
@@ -227,7 +227,7 @@
             }, "compiler": [7, ">= 4.0.0"], "main": function main(container, depth0, helpers, partials, data) {
                 var stack1;
 
-                return "<div class=\"dropdown-button\">\n  <div class=\"button-text\"></div>\n  <i class=\"icon-expand\" />\n</div>\n<input type=\"hidden\"" + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {}, depth0 != null ? depth0.name : depth0, { "name": "if", "hash": {}, "fn": container.program(1, data, 0), "inverse": container.noop, "data": data })) != null ? stack1 : "") + " />\n";
+                return "<div class=\"dropdown-button\">\n  <div class=\"button-text\"></div>\n  <i class=\"icons expand\">expand_more</i>\n</div>\n<input type=\"hidden\"" + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {}, depth0 != null ? depth0.name : depth0, { "name": "if", "hash": {}, "fn": container.program(1, data, 0), "inverse": container.noop, "data": data })) != null ? stack1 : "") + " />\n";
             }, "useData": true }),
         'spinner': handlebars.template({ "compiler": [7, ">= 4.0.0"], "main": function main(container, depth0, helpers, partials, data) {
                 return "<svg class=\"spinner\" width=\"65px\" height=\"65px\" viewBox=\"0 0 66 66\" xmlns=\"http://www.w3.org/2000/svg\">\n  <circle class=\"spinner-path\" fill=\"none\" stroke-width=\"6\" stroke-linecap=\"round\" cx=\"33\" cy=\"33\" r=\"30\"></circle>\n</svg>\n";
